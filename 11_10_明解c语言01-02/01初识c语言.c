559129af64bb215ffd1aa5c85b9a9da84b2b932f
wu-bao-12 <1023666344@qq.com>
2022-11-10 21:35:30 +0800

    .c

---
+++ 11_10_明解c语言01-02/01初识c语言.c
@@ -0,0 +1,101 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+//
+//#include<stdio.h>
+//int main()
+//{
+//	int a, b = 0;
+//	scanf("%d %d", &a, &b);
+//
+//	printf("%d-%d=%d", a,b, a - b);
+//	return 0;
+//}
+
+
+//
+#include<stdio.h>
+
+
+
+//int main()
+//{
+//	int a, b = 0;
+//	double c = 0.0;
+//
+//	printf("请输入两个整数\n");
+//	printf("整数1:");
+//	scanf("%d", &a);
+//
+//	printf("整数2:");
+//	scanf("%d", &b);
+//	c = a *1.0/ b * 100;
+//
+//	printf("%d占%d的%lf%%", a, b, c);
+//
+//
+//	return 0;
+//}
+
+
+//int main()
+//{
+//	printf("%%\n");
+//	puts("%%");
+//
+//	printf("\n");
+//	puts("");
+//
+//	return 0;
+//}
+
+//int main()
+//{
+//	int a, b = 0;
+//	
+//	printf("请输入整数：");
+//	scanf("%d", &a);
+//	printf("请输入整数：");
+//	scanf("%d", &b);
+//
+//	printf("他们的和是%d", a + b);
+//
+//	return 0;
+//}
+
+
+//int main()
+//{
+//	puts("天");
+//	puts("地");
+//	puts("人");
+//
+//
+//	return 0;
+//}
+
+
+
+
+//int main()
+//{
+//	int a = 0;
+//	printf("请输入一个整数："); scanf("%d", &a);
+//	printf("这个数加上12等于%d", a+12);
+//
+//	return 0;
+//}
+
+
+//int main()
+//{
+//	printf("喂！\n\n您好！\n再见！");
+//
+//	return 0;
+//}
+
+
+
+//2.int main()
+//{
+//	printf("天\n地\n人\n");
+//}
+
